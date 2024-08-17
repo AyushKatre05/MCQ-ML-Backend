84205dcdf960ce6d96c2a0551e6d35c43940df6f
Ayushatre05 <ayushkatre1801@gmail.com>
2024-08-17 23:15:20 +0530

    fix bugs

--- startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Install Python dependencies
-pip install -r requirements.txt
-
-# Download the spaCy model
-python -m spacy download en_core_web_sm
-
-# Start the Flask application
-gunicorn app:app
