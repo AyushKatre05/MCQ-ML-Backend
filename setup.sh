d02c23bcfe7089520b6de7dd66945a5ee73050f4
Ayushatre05 <ayushkatre1801@gmail.com>
2024-08-18 13:19:44 +0530

    last

--- setup.sh
+++ setup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 pip install -r requirements.txt
 python -m spacy download en_core_web_sm
-# Install maturin and Rust
-pip install maturin
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-export MATURIN_BUILD_DIR=/tmp
+pip list
